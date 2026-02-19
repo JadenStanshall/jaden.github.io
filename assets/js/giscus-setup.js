6c9552abb5717df2c5a7e916c9923dd6eacbdc9e
JadenStanshall <119129267+JadenStanshall@users.noreply.github.com>
2026-02-19 05:05:18 +0000

    Deploying to gh-pages from @ JadenStanshall/jaden.github.io@df6382ae07ad6f0b6a9d347474902bfe80658b58 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -1,15 +1,6 @@
 function determineGiscusTheme() {
   
-    let theme =
-      localStorage.getItem("theme") ||
-      document.documentElement.getAttribute("data-theme") ||
-      "system";
-
-    if (theme === "dark") return "dark";
-    if (theme === "light") return "light";
-
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    return prefersDark ? "dark" : "light";
+    return "light";
   
 }
 
@@ -18,7 +9,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "JadenStanshall/my_portfolio",
+    "data-repo": "JadenStanshall/jaden.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
