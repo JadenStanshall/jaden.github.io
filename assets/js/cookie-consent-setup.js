b51f1fa065cae31db7b5b6e43470fd4a74c5af21
JadenStanshall <119129267+JadenStanshall@users.noreply.github.com>
2026-02-19 07:04:37 +0000

    Deploying to gh-pages from @ JadenStanshall/jadenstanshall.github.io@168990e5342908ceb8827463fa5e8902118a90fd 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://jadenstanshall.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://jaden.github.io/#contact">contact us</a>.'
               }
             ]
           }
